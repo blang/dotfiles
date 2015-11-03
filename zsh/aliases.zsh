830c13599572044c3a65f2b020ec4539e691c19a
Benedikt Lang <github@benediktlang.de>
2015-11-03 17:27:31 +0100

    fix go aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,1 @@
-hash -d BLANG=$GOPATH/src/github.com/blang/
 hash -d UNI=$HOME/Dropbox/uni/ma_inf
--- zsh/golang.zsh
+++ zsh/golang.zsh
@@ -3,3 +3,4 @@ export GOROOT=$HOME/goruntime/go
 export GOPATH=$HOME/goprojects
 export PATH=$GOROOT/bin:$PATH
 export PATH=$GOPATH/bin:$PATH
+hash -d BLANG=$GOPATH/src/github.com/blang/
