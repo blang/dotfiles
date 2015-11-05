75aeabc31b1a2d4325b39010e3eae65187d2f9fa
Benedikt Lang <github@benediktlang.de>
2015-11-05 22:05:41 +0100

    add vim to zsh

---
+++ zsh/vimzsh.zsh
@@ -0,0 +1,19 @@
+# Activate Vim in zsh
+bindkey -v
+
+# Set lower timeout
+export KEYTIMEOUT=1
+
+# Use vim cli mode
+bindkey '^P' up-history
+bindkey '^N' down-history
+
+# backspace and ^h working even after
+# returning from command mode
+bindkey '^h' backward-delete-char
+
+# ctrl-w removed word backwards
+bindkey '^w' backward-kill-word
+
+# ctrl-r starts searching history backward
+bindkey '^r' history-incremental-search-backward
