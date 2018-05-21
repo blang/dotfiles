0e1b38d8e016b9258bafb7f7105e8b3961467b13
Benedikt Lang <github@benediktlang.de>
2018-05-21 11:19:03 +0200

    go dep and vim root command

--- zsh/golang.zsh
+++ zsh/golang.zsh
@@ -1,6 +1,8 @@
 # GOLang
 export GOROOT=$HOME/goruntime/go_latest
 export GOPATH=$HOME/goprojects
+export GODEP=$HOME/goruntime/dep_latest
 export PATH=$GOROOT/bin:$PATH
 export PATH=$GOPATH/bin:$PATH
+export PATH=$GODEP:$PATH
 hash -d BLANG=$GOPATH/src/github.com/blang/
